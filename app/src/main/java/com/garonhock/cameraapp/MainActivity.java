83889dc42b4cd0eff00dc1248b312bf6d56f490f
Garon Hock <garonhock@yahoo.com>
2019-07-08 11:48:01 -0700

    commit checkpoint 6

---
+++ app/src/main/java/com/garonhock/cameraapp/MainActivity.java
@@ -0,0 +1,110 @@
+package com.garonhock.cameraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView mImageView;
+    private Button mCameraButton;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    Uri mCurrentPhotoUri;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoUri = Uri.fromFile(image);
+        return image;
+    }
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+    private void savePictureIntent(){
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("photo_path", mCurrentPhotoUri.getPath());
+        setResult(Activity.RESULT_OK, resultIntent);
+
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            mImageView.setImageURI(mCurrentPhotoUri);
+
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == (R.id.camera_button)) {
+            dispatchTakePictureIntent();
+        }
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageView = (ImageView) findViewById(R.id.image_view);
+        mCameraButton = (Button) findViewById(R.id.camera_button);
+
+
+        mCameraButton.setOnClickListener(this);
+
+
+    }
+
+
+}
+
